644c89ea5116580273cec2061b800b8ef360b060
jenhoz <771970504@qq.com>
2020-08-12 10:14:12 +0800

    commit

--- src/part0/Question40.java
+++ src/part0/Question40.java
@@ -5,17 +5,20 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
-public class Question39 {
+public class Question40 {
 
 	public static void main(String[] args) {
-		int[] candidates = {2,3,6,7};
-		int target = 7;
-		System.out.println(combinationSum(candidates, target));
+		int[] candidates = {10,1,2,7,6,1,5};
+		int target = 8;
+		System.out.println(combinationSum2(candidates, target));
 	}
 	
-	public static List<List<Integer>> combinationSum(int[] candidates, int target) {
+	public static List<List<Integer>> combinationSum2(int[] candidates, int target) {
 		List<List<Integer>> res = new LinkedList<List<Integer>>();
 		int len = candidates.length;
+		if (len == 0) {
+			return res;
+		}
 		Arrays.sort(candidates);
 		dfs(candidates, len, 0, target, new LinkedList<>(), res);
 		return res;
@@ -24,15 +27,16 @@ public class Question39 {
 	private static void dfs(int[] candidates, int len, int begin, int residue, List<Integer> path, List<List<Integer>> res) {
 		if (residue == 0) {
 			res.add(new ArrayList<Integer>(path));
-			return;
 		}
 		for (int i = begin; i < len; i++) {
 			if (residue - candidates[i] < 0) {
 				break;
 			}
-//			residue = residue - candidates[i];
+			if (i > begin && candidates[i] == candidates[i - 1]) {
+				continue;
+			}
 			path.add(candidates[i]);
-			dfs(candidates, len, i, residue - candidates[i], path, res);
+			dfs(candidates, len, i + 1, residue - candidates[i], path, res);
 			path.remove(path.size() - 1);
 		}
 	}
